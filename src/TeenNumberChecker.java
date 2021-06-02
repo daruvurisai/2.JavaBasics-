3c1ce26dc04b06890b6b280b84f1fdf3a9dc02bd
daruvurisai <daruvurisai@gmail.com>
2021-06-02 16:11:19 -0500

    Method Chalenge

--- src/TeenNumberChecker.java
+++ src/TeenNumberChecker.java
@@ -1,2 +1,28 @@
 public class TeenNumberChecker {
+    public static boolean hasTeen(int a , int b , int c){
+        if ((a>=13&&a<=19)||(b>=13&&b<=19)||(c>=13&&c<=19)){
+            System.out.println(true);
+            return true;
+        }
+        else{
+            System.out.println(false);
+            return false;
+        }
+    }
+    public static boolean isTeen(int a){
+        if(a>=13&&a<=19){
+            System.out.println(true);
+            return true;
+        }
+        else{
+            System.out.println(false);
+            return false;
+        }
+    }
+
+    public static void main (String[] args){
+        isTeen(9);
+        hasTeen(1,15,23);
+    }
+
 }
